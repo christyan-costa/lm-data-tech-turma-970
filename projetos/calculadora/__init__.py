02406b268009f96ba20d9ddaf716e4271ce38bc9
christyan_costa <christyancosta@gmail.com>
2023-04-03 00:21:48 -0300

    criando feature "soma"

--- projetos/calculadora/__init__.py
+++ projetos/calculadora/__init__.py
@@ -1,4 +1,19 @@
 from funcoes import soma
 
 def calcule():
-    pass
+    a = input("Digite um valor numérico para 'a':")
+    try:
+        a = float(a)
+    except:
+        print(f"valor inválido")
+    
+    b = input("Digite um valor numérico para 'b':")
+    try:
+        b = float(b)
+    except:
+        print(f"valor inválido")
+
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        print(soma(a,b))
+    else:
+        raise TypeError(f"Os inbuts 'a' e 'b' devem ser números. \n recebido a = {a}, tipo = {type(a)} \n recebido b = {b}, tipo = {type(b)}")
